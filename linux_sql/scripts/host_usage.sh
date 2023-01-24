ee9b6e031378e888c916096933f9cc76c3d39fec
Enzuo Mou <570416229@qq.com>
2023-01-24 04:13:00 +0000

    modify host_info.sh and host_usage.sh

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -25,20 +25,19 @@ cpu_kernel=$(echo "$cpu_info" | grep "Cpu(s)" | awk '{print $4}')
 disk_io=$(vmstat --unit M -d | tail -1 | awk -v col="10" '{print $col}')
 disk_available=$(df -BM / | tail -1 | awk '{print $4}')
 timestamp=$(date +"%Y-%m-%d %T")
-#host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
-#host_id=$(psql -h localhost -p 5432 -d host_agent -U postgres -c "SELECT
-#id FROM host_info WHERE hostname='$hostname'")
+
 #Insert server usage into host_usage table
+#using insertion with subquery,also delete "M" in disk_available
+#delect number after points in usage of cpu_kernel and cpu_idle
 insert_stmt="INSERT INTO host_usage(
                 "timestamp", host_id, memory_free,
                 cpu_idle,cpu_kernel,disk_io,disk_available)
-              SELECT '$timestamp', id, '$memory_free', '${cpu_idle:0:2}',
-              '${cpu_kernel%.*}', '$disk_io', '${disk_available%%M}'
+              SELECT
+                '$timestamp', id, '$memory_free', '${cpu_idle%.*}',
+                '${cpu_kernel%.*}', '$disk_io', '${disk_available%%M}'
               FROM host_info
               WHERE hostname='$hostname'
               ";
-
-
 
 #set up env var
 export PGPASSWORD=$psql_password
