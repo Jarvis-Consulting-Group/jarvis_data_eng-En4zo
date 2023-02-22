14be2360c6b483152652c74072ac0ce061d130bf
Enzuo Mou <570416229@qq.com>
2023-02-22 18:54:00 +0000

    complete jdbc

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -10,11 +10,28 @@ public class JDBCExecutor {
         DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport","postgres","password");
         try{
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt(1));
-            }
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
+//            CustomerDAO customerDAO = new CustomerDAO(connection);
+//            Customer customer = new Customer();
+//            customer.setFirstName("John");
+//            customer.setLastName("Adams");
+//            customer.setEmail("jadams.wh.gov");
+//            customer.setPhone("(555) 555-9845");
+//            customer.setAddress("1234 Main St");
+//            customer.setCity("Arlington");
+//            customer.setState("VA");
+//            customer.setZipCode("42121");
+//
+//            Customer dbCustomer = customerDAO.create(customer);
+//            System.out.println(dbCustomer);
+//            dbCustomer = customerDAO.findById(dbCustomer.getId());
+//            System.out.println(dbCustomer);
+//            dbCustomer.setEmail("john,adans@wh.gov");
+//            dbCustomer = customerDAO.update(dbCustomer);
+//            System.out.println(dbCustomer);
+//            customerDAO.delete(dbCustomer.getId());
         }catch(SQLException e){
             e.printStackTrace();
         }
