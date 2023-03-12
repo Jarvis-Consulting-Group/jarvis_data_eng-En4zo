ee19727f1c3675f79f386698671f3df8897c8683
Enzuo Mou <570416229@qq.com>
2023-03-12 03:35:15 +0000

    update test cpde

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -91,31 +91,31 @@ public class TwitterHttpHelper implements HttpHelper{
 
     }
 
-//    public static void main(String[] args) throws Exception{
-//        String  CONSUMER_KEY = System.getenv("consumerKey");
-//        String  CONSUMER_SECRET = System.getenv("consumerSecret");
-//        String  ACCESS_TOKEN = System.getenv("accessToken");
-//        String  TOKEN_SECRET = System.getenv("tokenSecret");
-//
-//        System.out.println(CONSUMER_KEY + "|" + CONSUMER_SECRET + "|" + ACCESS_TOKEN + "|" + TOKEN_SECRET);
-//
-//        //HttpHelper httpHelper = new TwitterHttpHelper(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,TOKEN_SECRET);
-//        HttpHelper httpHelper = new TwitterHttpHelper();
-//        String text = "Hello World7799";
-////        HttpResponse response = httpHelper.httpPost(URI.create("https://api.twitter.com/2/tweets"), text);
-//        HttpResponse response = httpHelper.httpGet(URI.create("https://api.twitter.com/2/tweets?ids=1631026072627077120&tweet.fields=id,created_at,text,entities,public_metrics,geo&expansions=geo.place_id&place.fields=geo"));
-////        HttpResponse response = httpHelper.httpDelete(URI.create("https://api.twitter.com/2/tweets/1631375617609400320"));
-////        HttpResponse response = httpHelper.httpGet(URI.create("https://api.twitter.com/2/tweets?ids=1631058476355993605&tweet.fields=created_at,entities,public_metrics,geo"));
-//        String jsonStr;
-//        jsonStr = EntityUtils.toString(response.getEntity());
-//        System.out.println(jsonStr);
-//        Root root = JsonUtil.toObjectFromJson(jsonStr, Root.class);
-// //       System.out.println("this is tweet tostring method" + tweet.toString());
-//        System.out.println(JsonUtil.toJson(root,true,false));
-//
-////        System.out.println(tweet.getText());
-//
-//  //      System.out.println(response.getStatusLine().getStatusCode());
-//
-//    }
+    public static void main(String[] args) throws Exception{
+        String  CONSUMER_KEY = System.getenv("consumerKey");
+        String  CONSUMER_SECRET = System.getenv("consumerSecret");
+        String  ACCESS_TOKEN = System.getenv("accessToken");
+        String  TOKEN_SECRET = System.getenv("tokenSecret");
+
+        System.out.println(CONSUMER_KEY + "|" + CONSUMER_SECRET + "|" + ACCESS_TOKEN + "|" + TOKEN_SECRET);
+
+        //HttpHelper httpHelper = new TwitterHttpHelper(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,TOKEN_SECRET);
+        HttpHelper httpHelper = new TwitterHttpHelper();
+        String text = "Hello World7790099";
+        HttpResponse response = httpHelper.httpPost(URI.create("https://api.twitter.com/2/tweets"), text);
+//        HttpResponse response = httpHelper.httpGet(URI.create("https://api.twitter.com/2/tweets?ids=1632973093122588673&tweet.fields=id,created_at,text,entities,public_metrics,geo&expansions=geo.place_id&place.fields=geo"));
+//        HttpResponse response = httpHelper.httpDelete(URI.create("https://api.twitter.com/2/tweets/1631375617609400320"));
+//        HttpResponse response = httpHelper.httpGet(URI.create("https://api.twitter.com/2/tweets?ids=1631058476355993605&tweet.fields=created_at,entities,public_metrics,geo"));
+        String jsonStr;
+        jsonStr = EntityUtils.toString(response.getEntity());
+        System.out.println(jsonStr);
+        Root root = JsonUtil.toObjectFromJson(jsonStr, Root.class);
+ //       System.out.println("this is tweet tostring method" + tweet.toString());
+        System.out.println(JsonUtil.toJson(root,true,false));
+
+//        System.out.println(tweet.getText());
+
+  //      System.out.println(response.getStatusLine().getStatusCode());
+
+    }
 }
